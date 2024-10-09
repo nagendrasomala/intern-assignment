c4ee242026f29cbd55dad5da559338ca2571d8a8
nagendra <nagendrababusomala@gmail.com>
2024-10-09 20:02:48 +0530

    first commit

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ app.get('/hitme' , (req , res)=>{
 })
 
 
-app.listen(PORT , ()=>{
+app.listen(PORT , async ()=>{
+    await connectDb()
     console.log(`Server is running on port ${PORT}`);
-    connectDb()
+    
 })
